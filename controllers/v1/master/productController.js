2cf7dbb3938c2e5e6ccc436c124eace38354f28f
walid <mohamad.walid@spil.co.id>
2024-12-02 15:05:33 +0700

    --a: CRUD product

--- controllers/v1/master/productController.js
+++ controllers/v1/master/productController.js
@@ -2,7 +2,100 @@ const httpRes = require("../../../utils/httpRes");
 const model = require("../../../models/v1/index");
 const logger = require("../../../utils/logger");
 
+exports.create = async (req, res, next) => {
+  let response = {
+    rc: httpRes["HTTP_BADREQUEST"],
+    rd: httpRes[httpRes.HTTP_BADREQUEST],
+    data: [],
+  };
+
+  try {
+    let result = await model.productModel.read(req);
+
+    if (result["status"] == httpRes.HTTP_OK) {
+      response = {
+        rc: httpRes.HTTP_OK,
+        rd: httpRes[httpRes.HTTP_OK],
+        data: result["result"],
+      };
+    }
+
+    res.locals.status = result["status"];
+    res.locals.response = JSON.stringify(response);
+  } catch (error) {
+    res.locals.status = response["rc"];
+    res.locals.response = JSON.stringify(response);
+    response["data"] = error.message;
+
+    logger.error(response);
+  }
+
+  next();
+};
+
 exports.read = async (req, res, next) => {
+  let response = {
+    rc: httpRes["HTTP_BADREQUEST"],
+    rd: httpRes[httpRes.HTTP_BADREQUEST],
+    data: [],
+  };
+
+  try {
+    let result = await model.productModel.read(req);
+
+    if (result["status"] == httpRes.HTTP_OK) {
+      response = {
+        rc: httpRes.HTTP_OK,
+        rd: httpRes[httpRes.HTTP_OK],
+        data: result["result"],
+      };
+    }
+
+    res.locals.status = result["status"];
+    res.locals.response = JSON.stringify(response);
+  } catch (error) {
+    res.locals.status = response["rc"];
+    res.locals.response = JSON.stringify(response);
+    response["data"] = error.message;
+
+    logger.error(response);
+  }
+
+  next();
+};
+
+exports.update = async (req, res, next) => {
+  let response = {
+    rc: httpRes["HTTP_BADREQUEST"],
+    rd: httpRes[httpRes.HTTP_BADREQUEST],
+    data: [],
+  };
+
+  try {
+    let result = await model.productModel.read(req);
+
+    if (result["status"] == httpRes.HTTP_OK) {
+      response = {
+        rc: httpRes.HTTP_OK,
+        rd: httpRes[httpRes.HTTP_OK],
+        data: result["result"],
+      };
+    }
+
+    res.locals.status = result["status"];
+    res.locals.response = JSON.stringify(response);
+  } catch (error) {
+    res.locals.status = response["rc"];
+    res.locals.response = JSON.stringify(response);
+    response["data"] = error.message;
+
+    logger.error(response);
+  }
+
+  next();
+};
+
+exports.delete = async (req, res, next) => {
   let response = {
     rc: httpRes["HTTP_BADREQUEST"],
     rd: httpRes[httpRes.HTTP_BADREQUEST],
